79cd9eaf619dbf3f8d9defbfd7382d47b76aa60d
Nitzan Tal <nitzan.tal@amdocs.com>
2020-09-06 22:28:18 +0300

    introducing express body parser

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/', routes);
 
 const port = process.env.PORT || 5000;
